bab6c44f7ffb3dadaba418dc0d8719cc5ec5a65d
hathwayy <145585679+hathwayy@users.noreply.github.com>
2024-03-04 14:20:16 +0000

    Update urls.py

--- EasyStay/urls.py
+++ EasyStay/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('login/manager', views.manager_login, name="manager_login"),
     path('Uregister/', views.user_register, name="user_register"),
     path('Mregister/', views.manager_register, name="manager_register"),
-    path('search/', views.search_home, name="search_home"),
+   
+    path('', views.index, name='index'),
+    path('search/', views.search_rst, name='search'),
 ]
