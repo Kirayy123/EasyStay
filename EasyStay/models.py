364b9a7e7b0947a59a8260b5f65fb7aa68c89ef6
Yu Zhang <2861948Z@student.gla.ac.uk>
2024-03-07 14:41:38 +0000

    updated 3-7

--- EasyStay/models.py
+++ EasyStay/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 # Create your models here.
-
+'''__  User Table __'''
 class user(models.Model):
     user_id = models.CharField(max_length=10, default=None)
     username = models.CharField(max_length=50, default=None)
@@ -11,22 +11,22 @@ class user(models.Model):
     phone = models.CharField(max_length=50, default='+0000000000')
     password = models.CharField(max_length=50)
 
-
+'''__  Manager Table __'''
 class hotelmanager(models.Model):
     manage_id = models.CharField(max_length=10, default=None)
     email = models.EmailField(max_length=100, unique=True)
     phone = models.CharField(max_length=50, default='+0000000000')
     password = models.CharField(max_length=50)
 
-
+'''__  Hotel Table __'''
 class hotel(models.Model):
     manager = models.ForeignKey(hotelmanager, on_delete=models.CASCADE)
     hotel_id = models.CharField(max_length=10, default=None)
     name = models.CharField(max_length=100, default=None)
-    country = models.CharField(max_length=50, default=None)###_________________new
+    country = models.CharField(max_length=50, default=None)
     city = models.CharField(max_length=50, default=None)
     location = models.CharField(max_length=255, default=None)
-    postcode = models.CharField(max_length=50, default=None)###_________________newPost Code
+    postcode = models.CharField(max_length=50, default=None)
     email = models.EmailField(max_length=100, unique=True)
     phone = models.CharField(max_length=50, default=None)
     star = models.IntegerField(default=3)
@@ -34,40 +34,40 @@ class hotel(models.Model):
     facility = models.TextField(default=None)
     image = models.ImageField(upload_to='hotel_image', default=None)
 
-
+'''__  Room Type Table __'''
 class roomtype(models.Model):
     hotel = models.ForeignKey(hotel, on_delete=models.CASCADE)
     type = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=12, decimal_places=2, default=None)
-    guests = models.IntegerField(default=2)  ###_________________new
+    guests = models.IntegerField(default=2)
     facility = models.TextField(default=None)
     image = models.ImageField(upload_to='room_image',default=None)
 
-
+'''__  Room Table __'''
 class room(models.Model):
     type = models.ForeignKey(roomtype, on_delete=models.CASCADE)
     Room_number = models.CharField(max_length=10, default=None)
     availability = models.BooleanField(default=True)  # True for available, False for unavailable
 
-
+'''__  Booking Table __'''
 class booking(models.Model):
     user = models.ForeignKey(user, on_delete=models.CASCADE)
     room_number = models.ForeignKey(room, on_delete=models.CASCADE)
     ref_num = models.CharField(max_length=50, default=None)
     booking_date = models.DateTimeField(default=timezone.now)
 
-    from_date = models.DateField(default=None)####
-    to_date = models.DateField(default=None)####
+    from_date = models.DateField(default=None)
+    to_date = models.DateField(default=None)
 
     total_price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     is_paid = models.BooleanField(default=False)
     reserved_name = models.CharField(max_length=50, default=None)
     reserved_phone = models.CharField(max_length=50, default=None)
 
-    status = models.IntegerField(default='1')  # '1' for wait check-in//'2'checked in //'3'checked out
+    status = models.IntegerField(default='1')  # '1' for wait check-in//'2'checked in //'3'checked out // '4' not come
 
-    check_in_date = models.DateTimeField(null=True, blank=True)####
-    check_out_date = models.DateTimeField(null=True, blank=True)####
+    check_in_date = models.DateTimeField(null=True, blank=True)
+    check_out_date = models.DateTimeField(null=True, blank=True)
 
     review_star = models.IntegerField(null=True, blank=True)
     review_comment = models.TextField(null=True, blank=True)
--- EasyStay/urls.py
+++ EasyStay/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
                   # LoginPage
                   path('login/', views.login_home, name="login"),
                   path('login/manager', views.manager_login, name="manager_login"),
-                  path('managerlogout/', views.manager_logout, name="manager_logout"),
+                  path('logout/', views.logout, name="logout"),
                   path('Uregister/', views.user_register, name="user_register"),
                   path('Mregister/', views.manager_register, name="manager_register"),
+                  path('forgot_password/', views.forgot_password, name="forgot_password"),
+                  path('set_new_password/', views.set_new_password, name="set_new_password"),
 
                   # ManagerPage
                   path('manager/', views.manager_home, name="manager_home"),
@@ -28,11 +30,23 @@ urlpatterns = [
 
                   path('manager/booking', views.booking_list, name="bookings"),
 
+                  path('manager/review', views.review_list, name="review"),
+                  path('manager/reply_review/<int:id>/', views.reply_to_review, name="reply_to_review"),
+                  path('manager/review_filter', views.review_filter, name="review_filter"),
+                  path('booking_details/<int:booking_id>/', views.get_booking_details, name='get_booking_details'),
+
                   path('manager/checkin', views.check_in_list, name="checkin"),
                   path('manager/checkout', views.check_out_list, name="checkout"),
 
                   path('manager/profile', views.manager_profile, name="manager_profile"),
                   path('manager/profile/edit', views.manager_profile_edit, name="manager_profile_edit"),
                   path('manager/profile/change_password', views.manager_change_pw, name="change_password"),
+
+                  path('user/change_password', views.user_change_pw, name="user_change_password"),
+
+                  path('user/booking/<int:type_id>/', views.get_date_booking, name="user_booking"),
+                  path('booking/confirm/<int:booking_id>/', views.confirm_booking, name='confirm_booking'),
+                  path('booking/cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
+                  path('test/', views.search_home, name="test"),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
